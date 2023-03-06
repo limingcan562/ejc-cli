25576f12b52a0c5b636c866d55d3ad3dec361a9b
lMC <804666207@qq.com>
2023-03-06 18:55:32 +0800

    feat: excel转json功能完善中ing

--- src/index.js
+++ src/index.js
@@ -4,17 +4,24 @@ fs = require('fs-extra'),
 path = require('path'),
 {successLog, errorLog, warnLog} = require('./log/index');
 
-const 
-xlsxData = xlsx.parse(`${path.resolve(__dirname, '../xlsx/template.xlsx')}`),
-totalSheet = xlsxData.filter(item => item.data.length !== 0).length, // 去掉空的sheet
-taskArr = []; // 生成json的任务队列
-
-
-// console.log(xlsxData);
-// console.log(totalSheet);
-
 function create(options) {
-    console.log(options);
+    // console.log(options, path.resolve('.'));
+    const 
+    {input, output, keys, startLine} = options,
+    xlsxData = xlsx.parse(`${path.resolve('.', input)}`),
+    totalSheet = xlsxData.filter(item => item.data.length !== 0), // 整个Sheet数据（去掉空的sheet）
+    totalSheetNum = totalSheet.length, // 总共有几个表（去掉空的sheet）
+    taskArr = []; // 生成json的任务队列
+
+    for (let index = 0; index < totalSheet.length; index++) {
+        const 
+        everySheet = totalSheet[index],
+        {name: everySheeName, data} = everySheet,
+        renderData = data.splice(startLine - 1).filter(item => item.length !== 0);
+
+        console.log(renderData);
+    }
+    // console.log(totalSheet, totalSheetNum);
 }
 
 module.exports = create;
